bc9e807a099bfa42e72d60e66d515e4e0b09f97f
Akhil <akhil29122003penakacherla@gmail.com>
2024-12-17 18:32:33 -0500

    Create InventorySystem.h

---
+++ src/InventorySystem.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "ofMain.h"
+#include <vector>
+#include <string>
+#include <memory>
+
+using namespace std;
+
+class Item {
+public:
+	Item(const string& itemName, const string& itemDescription);
+	string getName();
+	string getDescription();
+
+private:
+	string name;
+	string description;
+};
+
+class Inventory {
+public:
+	void addItem(const shared_ptr<Item>& item);
+	bool removeItem(const string& itemName);
+	void displayInventory() const;
+	bool hasItem(const string& itemName) const;
+
+private:
+	vector<shared_ptr<Item>> items;
+};
