9e74500da9eca2a3e69f6bd57eaedd96e76128bd
Kro <124914389+Krowno04@users.noreply.github.com>
2024-12-19 22:15:40 -0500

    Removed unused lines

--- src/SoundPlay.h
+++ src/SoundPlay.h
@@ -14,10 +14,5 @@ class SoundPlay
 		//Example sound effect!
 		//ExampleSFX currently references an audio file called "MOUSECLICK.MP3". Change this if you want!
 		ofSoundPlayer	ExampleSFX;
-
-		//Currently unused
-		//float BgVolume = 100;
-		//float TxtVolume = 100;
-		//float SfxVolume = 100;
 };
 
